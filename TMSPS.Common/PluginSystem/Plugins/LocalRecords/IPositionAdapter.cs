e0af4cecc83a5884f9cdf3760373ad97c3bf4d1c
Scordo_cp <Scordo@users.noreply.github.com>
2009-02-08 19:34:00 +0000

    Implemted RecordAdapter and PositionAdapter and used them in LocalRecordsPlugin

--- TMSPS.Common/PluginSystem/Plugins/LocalRecords/IPositionAdapter.cs
+++ TMSPS.Common/PluginSystem/Plugins/LocalRecords/IPositionAdapter.cs
@@ -4,5 +4,6 @@ namespace TMSPS.Core.PluginSystem.Plugins.LocalRecords
 {
     public interface IPositionAdapter : IBaseAdapter
     {
+        void AddPosition(string login, string uniqueChallengeID, ushort position, ushort maxPosition);
     }
 }
