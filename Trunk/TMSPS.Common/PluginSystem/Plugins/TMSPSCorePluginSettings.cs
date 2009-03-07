8b944d9b54fd9281b40dfa2bad60bbfa4355eccc
Scordo_cp <SND\Scordo_cp>
2009-03-07 18:18:46 +0000

    Added possibility do deactivate join an dleave messages

--- Trunk/TMSPS.Common/PluginSystem/Plugins/TMSPSCorePluginSettings.cs
+++ Trunk/TMSPS.Common/PluginSystem/Plugins/TMSPSCorePluginSettings.cs
@@ -9,14 +9,18 @@ namespace TMSPS.Core.PluginSystem.Plugins
         #region Constants
 
         public const string JOIN_MESSAGE = "{[#ServerStyle]}>> {[#MessageStyle]}New Player: {[#HighlightStyle]}{[Nickname]}{[#MessageStyle]} Nation: {[#HighlightStyle]}{[Nation]}{[#MessageStyle]} Ladder: {[#HighlightStyle]}{[Ladder]}";
+        public const bool ENABLE_JOIN_MESSAGE = true;
         public const string LEAVE_MESSAGE = "{[#ServerStyle]}>> {[#HighlightStyle]}{[Nickname]}{[#MessageStyle]} has left the game.";
+        public const bool ENABLE_LEAVE_MESSAGE = true;
 
         #endregion
 
         #region Properties
 
+        public bool EnableJoinMessage { get; private set; }
         public string JoinMessage { get; private set; }
         public string LeaveMessage { get; private set; }
+        public bool EnableLeaveMessage { get; private set; }
 
         #endregion
 
@@ -32,7 +36,9 @@ namespace TMSPS.Core.PluginSystem.Plugins
             if (configDocument.Root == null)
                 throw new ConfigurationErrorsException("Could not find root node in file: " + xmlConfigurationFile);
 
+            result.EnableJoinMessage = ReadConfigBool(configDocument.Root, "EnableJoinMessage", ENABLE_JOIN_MESSAGE, xmlConfigurationFile);
             result.JoinMessage = ReadConfigString(configDocument.Root, "JoinMessage", JOIN_MESSAGE, xmlConfigurationFile);
+            result.EnableLeaveMessage = ReadConfigBool(configDocument.Root, "EnableLeaveMessage", ENABLE_LEAVE_MESSAGE, xmlConfigurationFile);
             result.LeaveMessage = ReadConfigString(configDocument.Root, "LeaveMessage", LEAVE_MESSAGE, xmlConfigurationFile);
             
 
