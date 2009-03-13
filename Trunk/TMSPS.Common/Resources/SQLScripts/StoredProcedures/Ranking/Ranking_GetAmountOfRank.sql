89fdb4cffdb1c671910e738b6ab8d99e47a4bc6f
Scordo_cp <SND\Scordo_cp>
2009-03-13 18:48:50 +0000

    Merged Ranking Changes into trunk

---
+++ Trunk/TMSPS.Common/Resources/SQLScripts/StoredProcedures/Ranking/Ranking_GetAmountOfRank.sql
@@ -0,0 +1,5 @@
+﻿Create FUNCTION [dbo].[Ranking_GetAmountOfRank](@playerID int, @rank int) returns int
+AS
+BEGIN
+	return (Select Count(*) FROM dbo.Ranking with (nolock) where PlayerID = @playerID and [Rank] = @rank)
+END
