2fbb7be0e1d38868d506f3fe57da19ba4cdf97f3
Scordo_cp <SND\Scordo_cp>
2009-03-24 23:22:36 +0000

    made it possible to install multiple instances of the tmsps deamon service on a single server

--- Trunk/TMSPS.Daemon/ProjectInstaller.cs
+++ Trunk/TMSPS.Daemon/ProjectInstaller.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
-using System.Linq;
-
 
 namespace TMSPS.Daemon
 {
@@ -14,6 +10,16 @@ namespace TMSPS.Daemon
 		public ProjectInstaller()
 		{
 			InitializeComponent();
+
+            const string paramName = "--ServiceName=";
+            int index = Environment.CommandLine.IndexOf(paramName, StringComparison.InvariantCultureIgnoreCase);
+            
+            if (index == -1)
+                return;
+
+            ServiceInstaller.ServiceName = Environment.CommandLine.Substring(index + paramName.Length).Trim();
+		    ServiceInstaller.DisplayName = ServiceInstaller.ServiceName;
+            Console.WriteLine("Installing as '" + ServiceInstaller.ServiceName + "'");
 		}
 	}
 }
