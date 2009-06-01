efdfbd61892b66d799bbbd7e6daad9ed29ec3f31
Scordo_cp <Scordo@users.noreply.github.com>
2009-06-01 19:23:06 +0000

    Optimized performance of ranking system

--- TMSPS.Common/Resources/SQLScripts/StoredProcedures/Ranking/Ranking_Deserialize_List.sql
+++ TMSPS.Common/Resources/SQLScripts/StoredProcedures/Ranking/Ranking_Deserialize_List.sql
@@ -1,38 +1,15 @@
-﻿set ANSI_NULLS ON
-set QUOTED_IDENTIFIER ON
-go
-
-
-ALTER PROCEDURE [dbo].[Ranking_Deserialize_List]
+﻿ALTER PROCEDURE [dbo].[Ranking_Deserialize_List]
 	@amountOfRankings int
 AS
 BEGIN
-	declare @challengesCount int
-	Select @challengesCount = Count(ID) FROM Challenge with (nolock)
-
-	;with MainQuery as
-	(
-		Select
-			ROW_NUMBER() OVER (order by (AVG([Rank]) + cast((@challengesCount+1) as float) / cast((Count([Rank])+1)as float) * cast((@challengesCount - Count([Rank])) as float)) ASC) [Rank],
-			PlayerID,
-			AVG([Rank]) as AverageRank,
-			Count([Rank]) as RecordsCount,
-			@challengesCount as ChallengesCount,
-			(AVG([Rank]) + cast((@challengesCount+1) as float) / cast((Count([Rank])+1)as float) * cast((@challengesCount - Count([Rank])) as float)) as Score
-		From
-			Ranking with (nolock)
-		GROUP By 
-			PlayerID
-	)
-
 	Select TOP(@amountOfRankings)
 		P.Nickname,
 		P.Login,
-		M.*
+		R.*
 	FROM
-		MainQuery M with(nolock)
+		Rank R with(nolock)
 	INNER JOIN
-		Player P with(nolock) on P.ID = m.PlayerID
+		Player P with(nolock) on P.ID = R.PlayerID
 	Order by
 		Rank Asc
 END
