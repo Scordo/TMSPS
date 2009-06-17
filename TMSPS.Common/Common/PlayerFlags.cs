1d6dd7bb186f7846ccca4cbd1d21d68c900f182c
Scordo_cp <Scordo@users.noreply.github.com>
2009-06-17 21:34:17 +0000

    Changed PlayerInfoChangedEventArgs to have type save properties for SpectatorStatus and Flags

---
+++ TMSPS.Common/Common/PlayerFlags.cs
@@ -0,0 +1,42 @@
+﻿using TMSPS.Core.Communication.ProxyTypes;
+
+namespace TMSPS.Core.Common
+{
+    public class PlayerFlags : IDump
+    {
+        public bool HasPlayerSlot { get; set; }
+        public bool IsServer { get; set; }
+        public bool IsManagedByAnOtherServer { get; set; }
+        public bool IsUsingStereoscopy { get; set; }
+        public bool IsPodiumReady { get; set; }
+        public bool IsReferee { get; set; }
+        public ForceSpectatorState ForceSpectator { get; set; }
+
+        public static PlayerFlags Parse(int spectatorStatus)
+        {
+            PlayerFlags result = new PlayerFlags();
+
+            result.HasPlayerSlot = (spectatorStatus / 1000000) > 0;
+            spectatorStatus -= (result.HasPlayerSlot ? 1 : 0) * 1000000;
+
+            result.IsServer = (spectatorStatus / 100000) > 0;
+            spectatorStatus -= (result.IsServer ? 1 : 0) * 100000;
+
+            result.IsManagedByAnOtherServer = (spectatorStatus / 10000) > 0;
+            spectatorStatus -= (result.IsManagedByAnOtherServer ? 1 : 0) * 10000;
+
+            result.IsUsingStereoscopy = (spectatorStatus / 1000) > 0;
+            spectatorStatus -= (result.IsUsingStereoscopy ? 1 : 0) * 1000;
+
+            result.IsPodiumReady = (spectatorStatus / 100) > 0;
+            spectatorStatus -= (result.IsPodiumReady ? 1 : 0) * 100;
+
+            result.IsReferee = (spectatorStatus / 10) > 0;
+            spectatorStatus -= (result.IsReferee ? 1 : 0) * 10;
+
+            result.ForceSpectator = (ForceSpectatorState) spectatorStatus;
+
+            return result;
+        }
+    }
+}
---
+++ TMSPS.Common/Common/PlayerSpectatorStatus.cs
@@ -0,0 +1,33 @@
+﻿namespace TMSPS.Core.Common
+{
+    public class PlayerSpectatorStatus : IDump
+    {
+        public bool IsSpectator { get; set; }
+        public bool IsTemporarySpectator { get; set; }
+        public bool IsPureSpectator { get; set; }
+        public bool IsAutoTarget { get; set; }
+        public int CurrentPlayerTargetID { get; set; }
+        public bool IsPCurrentPlayerTargetIDValid { get { return CurrentPlayerTargetID != 0 && CurrentPlayerTargetID != 255; } }
+
+        public static PlayerSpectatorStatus Parse(int spectatorStatus)
+        {
+            PlayerSpectatorStatus result = new PlayerSpectatorStatus();
+            
+            result.CurrentPlayerTargetID = spectatorStatus/10000;
+            spectatorStatus -= result.CurrentPlayerTargetID * 10000;
+
+            result.IsAutoTarget = (spectatorStatus / 1000) > 0;
+            spectatorStatus -= (result.IsAutoTarget ? 1 : 0) *  1000;
+
+            result.IsPureSpectator = (spectatorStatus / 100) > 0;
+            spectatorStatus -= (result.IsPureSpectator ? 1 : 0) * 100;
+
+            result.IsTemporarySpectator = (spectatorStatus / 10) > 0;
+            spectatorStatus -= (result.IsTemporarySpectator ? 1 : 0) * 10;
+
+            result.IsSpectator = (spectatorStatus > 0);
+
+            return result;
+        }
+    }
+}
