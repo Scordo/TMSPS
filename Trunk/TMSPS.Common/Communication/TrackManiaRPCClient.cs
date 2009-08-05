19553a47da5e1caf05382f9fb1d776b49622e80e
Scordo_cp <SND\Scordo_cp>
2009-08-05 20:18:15 +0000

    Implemented process termination when an error occurs for reading a message

--- Trunk/TMSPS.Common/Communication/TrackManiaRPCClient.cs
+++ Trunk/TMSPS.Common/Communication/TrackManiaRPCClient.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Windows.Forms;
 using System.Xml.Linq;
 using TMSPS.Core.Communication.EventArguments;
 using TMSPS.Core.Communication.ResponseHandling;
