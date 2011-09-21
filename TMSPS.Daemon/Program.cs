6ab4fa08d9159d8d884394d2f4f9fc8ccaf2895a
Scordo_cp <Scordo@users.noreply.github.com>
2011-09-21 22:06:21 +0000

    Started to add support for linux

--- TMSPS.Daemon/Program.cs
+++ TMSPS.Daemon/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Configuration.Install;
-using System.Reflection;
 using System.ServiceProcess;
 using System.Threading;
 using log4net;
@@ -28,29 +25,31 @@ namespace TMSPS.Daemon
 
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
-            if (Environment.UserInteractive)
-            {
-                if (args != null && args.Length > 0 && (args[0].Equals("/i", StringComparison.InvariantCultureIgnoreCase) || args[0].Equals("/install", StringComparison.InvariantCultureIgnoreCase) || args[0].Equals("-i", StringComparison.InvariantCultureIgnoreCase) || args[0].Equals("-install", StringComparison.InvariantCultureIgnoreCase)))
-                {
-                    InstallService(args);
-                    return;
-                }
+            RunPlatformDependent(Environment.OSVersion.Platform);
+        }
 
-                if (args != null && args.Length > 0 && (args[0].Equals("/u", StringComparison.InvariantCultureIgnoreCase) || args[0].Equals("/uninstall", StringComparison.InvariantCultureIgnoreCase) || args[0].Equals("-u", StringComparison.InvariantCultureIgnoreCase) || args[0].Equals("-uninstall", StringComparison.InvariantCultureIgnoreCase)))
-                {
-                    UnInstallService(args);
-                    return;
-                }
-
-                RunInConsoleMode();
-            }
-            else
+        private static void RunPlatformDependent(PlatformID platform)
+        {
+            switch (platform)
             {
-                RunAsService();
+                case PlatformID.Win32NT:
+                case PlatformID.Win32Windows:
+                case PlatformID.Win32S:
+                    if (Environment.UserInteractive)
+                        RunInTerminal();
+                    else
+                        RunAsWindowsService();
+                    break;
+                case PlatformID.Unix:
+                    RunInTerminal();
+                    break;
+                default:
+                    Console.WriteLine("Your System is not supported: " + platform);
+                    break;
             }
         }
 
-        private static void RunAsService()
+        private static void RunAsWindowsService()
         {
             int startUpIdleTime;
             int.TryParse(ConfigurationManager.AppSettings["StartupIdle"], out startUpIdleTime);
@@ -59,10 +58,10 @@ namespace TMSPS.Daemon
             ServiceBase.Run(new[] { new MainService() });
         }
 
-        private static void RunInConsoleMode()
+        private static void RunInTerminal()
         {
-            Console.WriteLine("Running in console mode.");
-            _log.Debug("Running in console mode.");
+            Console.WriteLine("Running in terminal mode.");
+            _log.Debug("Running in terminal mode.");
 
             _mainDaemon = new MainDaemon();
             _mainDaemon.Start();
@@ -70,64 +69,7 @@ namespace TMSPS.Daemon
             Console.ReadLine();
             _mainDaemon.Stop();
 
-            _log.Debug("Finished in console mode.");
-        }
-
-        private static void InstallService(string[] args)
-        {
-            string serviceName = GetServiceName(args);
-
-            List<string> parameters = new List<string>{Assembly.GetExecutingAssembly().Location, string.Format("/LogFile={0}", Assembly.GetExecutingAssembly().Location)};
-
-            if (!serviceName.IsNullOrTimmedEmpty())
-                parameters.Add(string.Format("--ServiceName={0}", serviceName));
-
-            try
-            {
-                ManagedInstallerClass.InstallHelper(parameters.ToArray());
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occured during install:");
-                Console.WriteLine(ex.ToString());
-            }
-        }
-
-        private static void UnInstallService(string[] args)
-        {
-            string serviceName = GetServiceName(args);
-
-            List<string> parameters = new List<string> {Assembly.GetExecutingAssembly().Location, "/u", string.Format("/LogFile={0}", Assembly.GetExecutingAssembly().Location)};
-
-            if (!serviceName.IsNullOrTimmedEmpty())
-                parameters.Add(string.Format("--ServiceName={0}", serviceName));
-
-            try
-            {
-                ManagedInstallerClass.InstallHelper(parameters.ToArray());            
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occured during uninstall:");
-                Console.WriteLine(ex.ToString());
-            }
-        }
-
-        private static string GetServiceName(string[] args)
-        {
-            if (args.Length > 1 && args[1].StartsWith("/sn=", StringComparison.InvariantCultureIgnoreCase))
-            {
-                string serviceName = args[1].Substring(4).Trim().Trim('"');
-                return serviceName.IsNullOrTimmedEmpty() ? null : serviceName;
-            }
-
-            if (args.Length > 1 && args[1].StartsWith("/servicename=", StringComparison.InvariantCultureIgnoreCase))
-            {
-                string serviceName = args[1].Substring(13).Trim().Trim('"');
-                return serviceName.IsNullOrTimmedEmpty() ? null : serviceName;
-            }
-
-            return null;
+            _log.Debug("Finished in terminal mode.");
         }
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
