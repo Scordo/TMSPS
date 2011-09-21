3fc8521734030af2439c1166e169708349813144
Scordo_cp <SND\Scordo_cp>
2011-09-21 22:06:00 +0000

    Fixed dedimania blacklist retrieval

--- Branches/VS2010Branch/TMSPS.Common/Common/BlackListReader.cs
+++ Branches/VS2010Branch/TMSPS.Common/Common/BlackListReader.cs
@@ -30,6 +30,7 @@ namespace TMSPS.Core.Common
         {
             string fileContent;
             WebClient webClient = new WebClient();
+            webClient.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
 
             try
             {
